6eacaaf309f1659b238d52bf379ab82ce5b99a18
Yuk1ra <159479985+Stanislav192@users.noreply.github.com>
2024-05-31 10:30:00 +0300

    Added_unit_tests

--- MiddleTest2/gallery/tests.py
+++ MiddleTest2/gallery/tests.py
@@ -1,3 +1,37 @@
 from django.test import TestCase
+from .models import Category, Image
+from datetime import date
+from django.core.files.uploadedfile import SimpleUploadedFile
 
-# Create your tests here.
+class CategoryModelTest(TestCase):
+    def setUp(self):
+        self.category = Category.objects.create(name='Test Category')
+
+    def test_category_creation(self):
+        self.assertIsInstance(self.category, Category)
+        self.assertEqual(self.category.name, 'Test Category')
+
+    def test_category_str_method(self):
+        self.assertEqual(str(self.category), 'Test Category')
+
+class ImageModelTest(TestCase):
+    def setUp(self):
+        self.category1 = Category.objects.create(name='Category 1')
+        self.category2 = Category.objects.create(name='Category 2')
+        self.image = SimpleUploadedFile(name='test_image.png', content=b'', content_type='image/png')
+        self.image_instance = Image.objects.create(
+            title='Test Image',
+            image=self.image,
+            created_date=date.today(),
+            age_limit=18
+        )
+        self.image_instance.categories.set([self.category1, self.category2])
+
+    def test_image_str_method(self):
+        self.assertEqual(str(self.image_instance), 'Test Image')
+
+    def test_image_categories(self):
+        categories = self.image_instance.categories.all()
+        self.assertEqual(categories.count(), 2)
+        self.assertIn(self.category1, categories)
+        self.assertIn(self.category2, categories)
